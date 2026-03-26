245863bd32626fc26de6bbefc527eb224a18a39f
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-03-26 18:53:04 -0300

    feat: Adiciona implementação para obter o usuário autenticado

---
+++ BackEnd/src/Gafel.Domain/Services/CurrentUser/ICurrentUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Gafel.Domain.Services.CurrentUser;
+
+public interface ICurrentUser
+{
+    long UserId { get; }
+}
---
+++ BackEnd/src/Gafel.Infrastructure/Services/CurrentUser/CurrentUser.cs
@@ -0,0 +1,19 @@
+﻿using Gafel.Domain.Services.CurrentUser;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Gafel.Infrastructure.Services.CurrentUser;
+
+public class CurrentUser(IHttpContextAccessor contextAccessor) : ICurrentUser
+{
+    private readonly IHttpContextAccessor _contextAccessor = contextAccessor;
+
+    public long UserId
+    {
+        get
+        {
+            var value = _contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Sid)!;
+            return long.Parse(value);
+        }
+    }
+}
