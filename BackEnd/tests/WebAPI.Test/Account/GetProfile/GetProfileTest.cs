abb744e5112237c171e9c80db0488be9856fd2c7
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-04-05 16:39:31 -0300

    test: Adiciona testes de integração para consulta ao perfil de usuário

---
+++ BackEnd/tests/WebAPI.Test/Account/GetProfile/GetProfileTest.cs
@@ -0,0 +1,43 @@
+﻿using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Net;
+using System.Text.Json;
+
+namespace WebAPI.Test.Account.GetProfile;
+
+public class GetProfileTest : GafelClassFixture
+{
+    private const string METHOD = "me";
+
+    private readonly long _userId;
+    private readonly string _userEmail;
+    private readonly string _personFullName;
+    private readonly string _personCpf;
+    public GetProfileTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userId = factory.GetUserId();
+        _userEmail = factory.GetUserEmail();
+        _personFullName = factory.GetPersonFullName();
+        _personCpf = factory.GetPersonCpf();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _userId);
+
+        // Act
+        var response = await DoGet(method: METHOD, token: token);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("email").GetString().ShouldBe(_userEmail);
+        responseData.RootElement.GetProperty("fullName").GetString().ShouldBe(_personFullName);
+        responseData.RootElement.GetProperty("cpf").GetString().ShouldBe(_personCpf);
+    }
+}
---
+++ BackEnd/tests/WebAPI.Test/Account/GetProfile/GetProfileTestInvalidTokenTest.cs
@@ -0,0 +1,36 @@
+﻿using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Net;
+
+namespace WebAPI.Test.Account.GetProfile;
+
+public class GetProfileTestInvalidTokenTest(CustomWebApplicationFactory factory) : GafelClassFixture(factory)
+{
+    private const string METHOD = "me";
+
+    [Fact]
+    public async Task Error_Token_Invalid()
+    {
+        var response = await DoGet(method: METHOD, token: "invalidToken");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Without_Token()
+    {
+        var response = await DoGet(method: METHOD, token: string.Empty);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Token_With_User_NotFound()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: 10);
+
+        var response = await DoGet(method: METHOD, token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
