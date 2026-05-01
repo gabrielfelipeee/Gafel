8edd4ea53d582f0ffdb2f1f6c35b7b2f4b1bd2b5
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-05-01 10:57:18 -0300

    test: Adiciona testes de integração para cadastro de Categoria

---
+++ BackEnd/tests/WebAPI.Test/Category/Register/RegisterCategoryInvalidToken.cs
@@ -0,0 +1,42 @@
+﻿using CommonTestUtilities.Commands;
+using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Net;
+
+namespace WebAPI.Test.Category.Register;
+
+public class RegisterCategoryInvalidToken(CustomWebApplicationFactory factory) : GafelClassFixture(factory)
+{
+    private const string METHOD = "categories";
+
+    [Fact]
+    public async Task Error_Token_Invalid()
+    {
+        var request = CategoryCommandBuilder.Build();
+
+        var response = await DoPost(method: METHOD, request: request, token: "invalidToken");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Without_Token()
+    {
+        var request = CategoryCommandBuilder.Build();
+
+        var response = await DoPost(method: METHOD, request: request, token: string.Empty);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Token_With_User_NotFound()
+    {
+        var request = CategoryCommandBuilder.Build();
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: 10);
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
---
+++ BackEnd/tests/WebAPI.Test/Category/Register/RegisterCategoryTest.cs
@@ -0,0 +1,73 @@
+﻿using CommonTestUtilities.Commands;
+using CommonTestUtilities.Tokens;
+using Gafel.Domain.Enums;
+using Gafel.Domain.Resources;
+using Shouldly;
+using System.Net;
+using WebAPI.Test.Assertions;
+
+namespace WebAPI.Test.Category.Register;
+
+public class RegisterCategoryTest : GafelClassFixture
+{
+    private const string METHOD = "categories";
+
+    private readonly string _personCpf;
+    private readonly long _userId;
+
+    public RegisterCategoryTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _personCpf = factory.GetPersonCpf();
+        _userId = factory.GetUserId();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange
+        var request = CategoryCommandBuilder.Build();
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _userId);
+
+        // Act
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var request = CategoryCommandBuilder.Build();
+        request.Name = string.Empty;
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _userId);
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        await response.ShouldHaveSingleValidationError(field: "name", expectedMessage: ResourceMessagesException.CATEGORY_NAME_EMPTY);
+    }
+
+    [Fact]
+    public async Task Error_Icon_Empty()
+    {
+        var request = CategoryCommandBuilder.Build();
+        request.Icon = string.Empty;
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _userId);
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        await response.ShouldHaveSingleValidationError(field: "icon", expectedMessage: ResourceMessagesException.CATEGORY_ICON_EMPTY);
+    }
+
+    [Fact]
+    public async Task Error_Type_Invalid()
+    {
+        var request = CategoryCommandBuilder.Build();
+        request.Type = (CategoryType)100;
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _userId);
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        await response.ShouldHaveSingleValidationError(field: "type", expectedMessage: ResourceMessagesException.CATEGORY_TYPE_INVALID);
+    }
+}
