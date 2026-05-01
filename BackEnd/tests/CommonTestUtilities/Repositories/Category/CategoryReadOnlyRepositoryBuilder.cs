0ccad25b32852deb241a7f995ad77e21d1ddba83
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-05-01 17:52:26 -0300

    test: Adiciona testes para use case de buscar categoria por id

---
+++ BackEnd/tests/CommonTestUtilities/Repositories/Category/CategoryReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Gafel.Domain.Repositories.Category;
+using Moq;
+
+namespace CommonTestUtilities.Repositories.Category;
+
+public class CategoryReadOnlyRepositoryBuilder
+{
+    private readonly Mock<ICategoryReadOnlyRepository> _repository;
+    public CategoryReadOnlyRepositoryBuilder() => _repository = new Mock<ICategoryReadOnlyRepository>();
+
+    public ICategoryReadOnlyRepository Build() => _repository.Object;
+
+    public void GetById(Gafel.Domain.Entities.Person person, Gafel.Domain.Entities.Category category)
+        => _repository.Setup(repository => repository.GetById(person, category.Id)).ReturnsAsync(category);
+}
---
+++ BackEnd/tests/UseCases.Test/Category/GetById/GetCategoryByIdUseCaseTest.cs
@@ -0,0 +1,79 @@
+﻿using CommonTestUtilities.Dtos;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories.Category;
+using CommonTestUtilities.Repositories.Person;
+using CommonTestUtilities.Services.CurrentUser;
+using Gafel.Application.Exceptions;
+using Gafel.Application.UseCases.Category.GetById;
+using Gafel.Domain.Dtos;
+using Gafel.Domain.Resources;
+using Shouldly;
+using System.Net;
+
+namespace UseCases.Test.Category.GetById;
+
+public class GetCategoryByIdUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange 
+        var user = UserDtoBuilder.Build();
+        var person = PersonBuilder.Build();
+        var category = CategoryBuilder.Build(person);
+
+        var useCase = CreateUseCase(user: user, category: category, person: person);
+
+        //Act 
+        async Task act() => await useCase.Execute(category.Id);
+
+        // Assert
+        await Should.NotThrowAsync(act);
+    }
+
+    [Fact]
+    public async Task Error_Category_NotFound()
+    {
+        var user = UserDtoBuilder.Build();
+        var useCase = CreateUseCase(user: user);
+
+        var exception = await Should.ThrowAsync<PersonNotFoundException>(useCase.Execute(categoryId: 100));
+
+        exception.GetErrorTitle().ShouldBe(ResourceMessagesException.EXCEPTION_PERSON_NOT_FOUND_TITLE);
+        exception.GetErrorDetail().ShouldBe(ResourceMessagesException.EXCEPTION_PERSON_NOT_FOUND_DETAIL);
+        exception.GetStatusCode().ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task Error_Person_NotFound()
+    {
+        var user = UserDtoBuilder.Build();
+        var useCase = CreateUseCase(user: user);
+
+        var exception = await Should.ThrowAsync<PersonNotFoundException>(useCase.Execute(categoryId: 1));
+
+        exception.GetErrorTitle().ShouldBe(ResourceMessagesException.EXCEPTION_PERSON_NOT_FOUND_TITLE);
+        exception.GetErrorDetail().ShouldBe(ResourceMessagesException.EXCEPTION_PERSON_NOT_FOUND_DETAIL);
+        exception.GetStatusCode().ShouldBe(HttpStatusCode.NotFound);
+    }
+
+
+    private static GetCategoryByIdUseCase CreateUseCase(UserDto user, Gafel.Domain.Entities.Category? category = null, Gafel.Domain.Entities.Person? person = null)
+    {
+        var currentUser = CurrentUserBuilder.Build(user);
+
+        var personReadOnlyRepository = new PersonReadOnlyRepositoryBuilder();
+        if (person is not null)
+            personReadOnlyRepository.GetByUserId(person);
+
+        var categoryReadOnlyRepository = new CategoryReadOnlyRepositoryBuilder();
+        if (person is not null && category is not null)
+            categoryReadOnlyRepository.GetById(person, category);
+
+        return new(
+            currentUser,
+            personReadOnlyRepository.Build(),
+            categoryReadOnlyRepository.Build()
+        );
+    }
+}
