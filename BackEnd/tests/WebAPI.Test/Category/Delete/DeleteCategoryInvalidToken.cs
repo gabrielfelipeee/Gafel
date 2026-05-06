27cc5c3be450055aa48d37087402b7faf0093624
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-05-05 21:00:21 -0300

    test: Adiciona testes de integração para exclusão de categorias

---
+++ BackEnd/tests/WebAPI.Test/Category/Delete/DeleteCategoryInvalidToken.cs
@@ -0,0 +1,39 @@
+﻿using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Net;
+
+namespace WebAPI.Test.Category.Delete;
+
+public class DeleteCategoryInvalidToken : GafelClassFixture
+{
+    private const string METHOD = "categories";
+
+    private readonly Gafel.Domain.Entities.Category _category;
+    public DeleteCategoryInvalidToken(CustomWebApplicationFactory factory) : base(factory) => _category = factory.GetCategory();
+
+    [Fact]
+    public async Task Error_Token_Invalid()
+    {
+        var response = await DoGet(method: $"{METHOD}/{_category.Id}", token: "invalidToken");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Without_Token()
+    {
+        var response = await DoGet(method: $"{METHOD}/{_category.Id}", token: string.Empty);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Token_With_User_NotFound()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: 10);
+
+        var response = await DoGet(method: $"{METHOD}/{_category.Id}", token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
---
+++ BackEnd/tests/WebAPI.Test/Category/Delete/DeleteCategoryTest.cs
@@ -0,0 +1,55 @@
+﻿using CommonTestUtilities.Tokens;
+using Gafel.Domain.Dtos;
+using Gafel.Domain.Resources;
+using Shouldly;
+using System.Net;
+using System.Text.Json;
+
+namespace WebAPI.Test.Category.Delete;
+
+public class DeleteCategoryTest : GafelClassFixture
+{
+    private const string METHOD = "categories";
+
+    private readonly UserDto _user;
+    private readonly Gafel.Domain.Entities.Category _category;
+    public DeleteCategoryTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _user = factory.GetUser();
+        _category = factory.GetCategory();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        // Act
+        var response = await DoDelete(method: $"{METHOD}/{_category.Id}", token: token);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+        response = await DoGet($"{METHOD}/{_category.Id}", token: token);
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task Error_Category_NotFound()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        var response = await DoDelete(method: $"{METHOD}/100", token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+
+        await using var stream = await response.Content.ReadAsStreamAsync();
+        var json = await JsonDocument.ParseAsync(stream);
+
+        json.RootElement.GetProperty("title").GetString().ShouldBe(ResourceMessagesException.EXCEPTION_NOT_FOUND_TITLE);
+        json.RootElement.GetProperty("detail").GetString().ShouldBe(ResourceMessagesException.CATEGORY_NOT_FOUND);
+        json.RootElement.GetProperty("status").GetInt32().ShouldBe((int)HttpStatusCode.NotFound);
+
+    }
+}
