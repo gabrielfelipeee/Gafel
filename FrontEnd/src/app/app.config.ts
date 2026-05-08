a071e301230bed21241d4c65e8b9c1bb31bb3ed5
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-05-07 21:21:54 -0300

    style (web): Instala Tailwind CSS e configura paleta de cores e tipografia

--- FrontEnd/src/app/app.config.ts
+++ FrontEnd/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(), 
+    provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ],
 };
