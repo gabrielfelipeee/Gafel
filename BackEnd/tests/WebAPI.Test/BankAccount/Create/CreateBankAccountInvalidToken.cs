ead1a1ec4fe6675e41039c9ca4877a611b702714
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-06-13 18:25:24 -0300

    test (api): Adiciona testes de integração para o endpoint de criar conta bancária

---
+++ BackEnd/tests/WebAPI.Test/BankAccount/Create/CreateBankAccountInvalidToken.cs
@@ -0,0 +1,42 @@
+﻿using CommonTestUtilities.Commands;
+using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Net;
+
+namespace WebAPI.Test.BankAccount.Create;
+
+public class CreateBankAccountInvalidToken(CustomWebApplicationFactory factory) : GafelClassFixture(factory)
+{
+    private const string METHOD = "bankaccounts";
+
+    [Fact]
+    public async Task Error_Token_Invalid()
+    {
+        var request = BankAccountCommandBuilder.Build();
+
+        var response = await DoPost(method: METHOD, request: request, token: "invalidToken");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Without_Token()
+    {
+        var request = BankAccountCommandBuilder.Build();
+
+        var response = await DoPost(method: METHOD, request: request, token: string.Empty);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Token_With_User_NotFound()
+    {
+        var request = BankAccountCommandBuilder.Build();
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: 10);
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
---
+++ BackEnd/tests/WebAPI.Test/BankAccount/Create/CreateBankAccountTest.cs
@@ -0,0 +1,70 @@
+﻿using CommonTestUtilities.Commands;
+using CommonTestUtilities.Tokens;
+using Gafel.Domain.Dtos;
+using Gafel.Domain.Enums;
+using Gafel.Domain.Resources;
+using Shouldly;
+using System.Net;
+using WebAPI.Test.Assertions;
+
+namespace WebAPI.Test.BankAccount.Create;
+
+public class CreateBankAccountTest : GafelClassFixture
+{
+    private const string METHOD = "bankaccounts";
+
+    private readonly UserDto _user;
+
+    public CreateBankAccountTest(CustomWebApplicationFactory factory) : base(factory) => _user = factory.GetUser();
+
+
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange
+        var request = BankAccountCommandBuilder.Build();
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        // Act
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var request = BankAccountCommandBuilder.Build();
+        request.Name = string.Empty;
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        await response.ShouldHaveSingleValidationError(field: "name", expectedMessage: ResourceMessagesException.BANK_ACCOUNT_NAME_EMPTY);
+    }
+
+    [Fact]
+    public async Task Error_InitialBalance_Negative()
+    {
+        var request = BankAccountCommandBuilder.Build();
+        request.InitialBalance = -100;
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        await response.ShouldHaveSingleValidationError(field: "initialBalance", expectedMessage: ResourceMessagesException.INVALID_INITIAL_BALANCE);
+    }
+
+    [Fact]
+    public async Task Error_Type_Invalid()
+    {
+        var request = BankAccountCommandBuilder.Build();
+        request.Type = (BankAccountType)100;
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        await response.ShouldHaveSingleValidationError(field: "type", expectedMessage: ResourceMessagesException.BANK_ACCOUNT_TYPE_INVALID);
+    }
+}
