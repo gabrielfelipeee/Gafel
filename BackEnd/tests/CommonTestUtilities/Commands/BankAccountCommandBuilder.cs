8b8f29d5f32fbeae745997d8558cddcaf90725b3
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-06-13 11:26:57 -0300

    test (api): Adiciona testes para o validador de criar conta bancária

---
+++ BackEnd/tests/CommonTestUtilities/Commands/BankAccountCommandBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using Gafel.Application.UseCases.BankAccount.Shared.Commands;
+using Gafel.Domain.Enums;
+
+namespace CommonTestUtilities.Commands;
+
+public class BankAccountCommandBuilder
+{
+    public static BankAccountCommand Build()
+    {
+        return new Faker<BankAccountCommand>("pt_BR")
+        .RuleFor(bankAccount => bankAccount.Name, faker => faker.Company.CompanyName())
+        .RuleFor(bankAccount => bankAccount.InitialBalance, faker => faker.Finance.Amount(0, 10000))
+        .RuleFor(bankAccount => bankAccount.Type, faker => faker.PickRandom<BankAccountType>());
+    }
+}
---
+++ BackEnd/tests/Validators.Test/BankAccount/Create/CreateBankAccountValidatorTest.cs
@@ -0,0 +1,67 @@
+﻿using CommonTestUtilities.Commands;
+using FluentValidation.TestHelper;
+using Gafel.Application.UseCases.BankAccount.Shared.Validators;
+using Gafel.Domain.Enums;
+using Gafel.Domain.Resources;
+using Shouldly;
+
+namespace Validators.Test.BankAccount.Create;
+
+public class CreateBankAccountValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var request = BankAccountCommandBuilder.Build();
+        var validator = new BankAccountValidator();
+
+        // Act
+        var result = validator.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void Error_Name_Empty()
+    {
+        var request = BankAccountCommandBuilder.Build();
+        request.Name = string.Empty;
+        var validator = new BankAccountValidator();
+
+        var result = validator.TestValidate(request);
+
+        result.Errors.ShouldHaveSingleItem();
+        result.ShouldHaveValidationErrorFor(x => x.Name)
+            .WithErrorMessage(ResourceMessagesException.BANK_ACCOUNT_NAME_EMPTY);
+    }
+
+    [Fact]
+    public void Error_InitialBalance_Negative()
+    {
+        var request = BankAccountCommandBuilder.Build();
+        request.InitialBalance = -100;
+        var validator = new BankAccountValidator();
+
+        var result = validator.TestValidate(request);
+
+        result.Errors.ShouldHaveSingleItem();
+        result.ShouldHaveValidationErrorFor(x => x.InitialBalance)
+            .WithErrorMessage(ResourceMessagesException.INVALID_INITIAL_BALANCE);
+    }
+
+    [Fact]
+    public void Error_Type_Invalid()
+    {
+        var request = BankAccountCommandBuilder.Build();
+        request.Type = (BankAccountType)100;
+        var validator = new BankAccountValidator();
+
+        var result = validator.TestValidate(request);
+
+        result.Errors.ShouldHaveSingleItem();
+        result.ShouldHaveValidationErrorFor(x => x.Type)
+            .WithErrorMessage(ResourceMessagesException.BANK_ACCOUNT_TYPE_INVALID);
+    }
+}
