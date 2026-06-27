d9b7cf85861ea5f94961f1eed30ad95504691a99
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-06-27 13:49:05 -0300

    test (api): Adiciona testes de integração para o endpoint de filtrar contas bancárias

---
+++ BackEnd/tests/WebAPI.Test/BankAccount/Filter/FilterBankAccountInvalidToken.cs
@@ -0,0 +1,37 @@
+﻿using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Net;
+
+namespace WebAPI.Test.BankAccount.Filter;
+
+public class FilterBankAccountInvalidToken : GafelClassFixture
+{
+    private const string METHOD = "bankaccounts";
+    public FilterBankAccountInvalidToken(CustomWebApplicationFactory factory) : base(factory) { }
+
+    [Fact]
+    public async Task Error_Token_Invalid()
+    {
+        var response = await DoGet(method: METHOD, token: "invalidToken");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Without_Token()
+    {
+        var response = await DoGet(method: METHOD, token: string.Empty);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Token_With_User_NotFound()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: 10);
+
+        var response = await DoGet(method: METHOD, token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
---
+++ BackEnd/tests/WebAPI.Test/BankAccount/Filter/FilterBankAccountTest.cs
@@ -0,0 +1,51 @@
+﻿using CommonTestUtilities.Tokens;
+using Gafel.Domain.Dtos;
+using Gafel.Domain.Dtos.QueryParams;
+using Shouldly;
+using System.Net;
+using System.Text.Json;
+
+namespace WebAPI.Test.BankAccount.Filter;
+
+public class FilterBankAccountTest : GafelClassFixture
+{
+    private const string METHOD = "bankaccounts";
+
+    private readonly UserDto _user;
+    public FilterBankAccountTest(CustomWebApplicationFactory factory) : base(factory) => _user = factory.GetUser();
+
+
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        // Act -> sem query params (offset/limit)
+        var response = await DoGet(method: METHOD, token: token);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        await using var stream = await response.Content.ReadAsStreamAsync();
+        var json = await JsonDocument.ParseAsync(stream);
+
+        int total = json.RootElement.GetProperty("total").GetInt32();
+        total.ShouldBe(1);
+
+        json.RootElement.GetProperty("offset").GetInt32().ShouldBe(0);
+        json.RootElement.GetProperty("limit").GetInt32().ShouldBe(total);
+        json.RootElement.GetProperty("items").GetArrayLength().ShouldBe(total);
+    }
+
+    [Fact]
+    public async Task Error_Type_Invalid()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+        var url = $"{METHOD}?{nameof(FilterBankAccountQueryParams.Type)}=invalid";
+
+        var response = await DoGet(method: url, token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+}
