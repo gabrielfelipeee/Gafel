6195e33ee25726c8e760957c80a2c2968549206a
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-06-26 21:25:42 -0300

    test (api): Adiciona testes de integração para o endpoint de excluir conta bancária

---
+++ BackEnd/tests/WebAPI.Test/BankAccount/Delete/DeleteBankAccountTest.cs
@@ -0,0 +1,55 @@
+﻿using CommonTestUtilities.Tokens;
+using Gafel.Domain.Dtos;
+using Gafel.Domain.Resources;
+using Shouldly;
+using System.Net;
+using System.Text.Json;
+
+namespace WebAPI.Test.BankAccount.Delete;
+
+public class DeleteBankAccountTest : GafelClassFixture
+{
+    private const string METHOD = "bankaccounts";
+
+    private readonly UserDto _user;
+    private readonly (Gafel.Domain.Entities.BankAccount entity, string obfuscatedId) _bankAccount;
+    public DeleteBankAccountTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _user = factory.GetUser();
+        _bankAccount = factory.GetBankAccount();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        // Act
+        var response = await DoDelete(method: $"{METHOD}/{_bankAccount.obfuscatedId}", token: token);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+        response = await DoGet($"{METHOD}/{_bankAccount.obfuscatedId}", token: token);
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task Error_BankAccount_NotFound()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userId: _user.Id);
+
+        var response = await DoDelete(method: $"{METHOD}/100", token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+
+        await using var stream = await response.Content.ReadAsStreamAsync();
+        var json = await JsonDocument.ParseAsync(stream);
+
+        json.RootElement.GetProperty("title").GetString().ShouldBe(ResourceMessagesException.EXCEPTION_NOT_FOUND_TITLE);
+        json.RootElement.GetProperty("detail").GetString().ShouldBe(ResourceMessagesException.BANK_ACCOUNT_NOT_FOUND);
+        json.RootElement.GetProperty("status").GetInt32().ShouldBe((int)HttpStatusCode.NotFound);
+
+    }
+}
