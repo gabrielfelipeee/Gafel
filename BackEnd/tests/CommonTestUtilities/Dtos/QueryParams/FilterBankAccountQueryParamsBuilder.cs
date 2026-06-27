f558a6c15ce268bacb2a95ee27012da2eaf92f90
Gabriel Felipe <gabrielfeliperodrigues78@gmail.com>
2026-06-27 13:38:34 -0300

    test (api): Adiciona testes para o validador de query params de filtrar contas bancárias

---
+++ BackEnd/tests/CommonTestUtilities/Dtos/QueryParams/FilterBankAccountQueryParamsBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using Gafel.Domain.Dtos.QueryParams;
+using Gafel.Domain.Enums;
+
+namespace CommonTestUtilities.Dtos.QueryParams;
+
+public class FilterBankAccountQueryParamsBuilder
+{
+    public static FilterBankAccountQueryParams Build(
+        int? offset = null,
+        int? limit = null,
+        BankAccountType? type = null,
+        bool nullOffset = false,
+        bool nullLimit = false)
+    {
+        return new Faker<FilterBankAccountQueryParams>()
+            .RuleFor(x => x.Offset, faker => nullOffset ? null : offset ?? faker.Random.Int(0, 100))
+            .RuleFor(x => x.Limit, faker => nullLimit ? null : limit ?? faker.Random.Int(1, 100))
+            .RuleFor(x => x.Type, faker => type ?? faker.PickRandom<BankAccountType>());
+    }
+}
---
+++ BackEnd/tests/Validators.Test/BankAccount/Filter/FilterBankAccountQueryParamsValidatorTest.cs
@@ -0,0 +1,37 @@
+﻿using CommonTestUtilities.Dtos.QueryParams;
+using FluentValidation.TestHelper;
+using Gafel.Application.UseCases.BankAccount.Filter;
+using Gafel.Domain.Enums;
+using Gafel.Domain.Resources;
+using Shouldly;
+
+namespace Validators.Test.BankAccount.Filter;
+
+public class FilterBankAccountQueryParamsValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var request = FilterBankAccountQueryParamsBuilder.Build();
+        var validator = new FilterBankAccountQueryParamsValidator();
+
+        // Act
+        var result = validator.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void Error_Type_Invalid()
+    {
+        var request = FilterBankAccountQueryParamsBuilder.Build(type: (BankAccountType)100);
+        var validator = new FilterBankAccountQueryParamsValidator();
+
+        var result = validator.TestValidate(request);
+
+        result.Errors.ShouldHaveSingleItem();
+        result.ShouldHaveValidationErrorFor(x => x.Type).WithErrorMessage(ResourceMessagesException.BANK_ACCOUNT_TYPE_INVALID);
+    }
+}
